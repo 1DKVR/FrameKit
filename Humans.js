6ab4a3e987d651548306688c861e57490de915b5
1Dkvr <1D.KVR.user@gmail.com>
2026-03-08 21:09:28 +0100

    Add Humans.js to bridge global identity with context

---
+++ Humans.js
@@ -0,0 +1,7 @@
+/**
+ * @file Humans.js (Repo2 version)
+ * @description Bridges global identity with local project context.
+ */
+import { Ø1D } from "https://1dkvr.github.io/FrameKit/Human.js";
+Ø1D.attach("FrameKit");
+export { Ø1D };
