9a068f8cde7a8fb3c952a281db548cf8933ef554
1Dkvr <1D.KVR.user@gmail.com>
2026-03-09 01:16:58 +0100

    Dynamic identity resolver

--- Humans.js
+++ Humans.js
@@ -1,6 +1,6 @@
 /**
- * @file Humans.js (Repo2 version)
- * @description Bridges global identity with local project context.
+ * @file Humans.js
+ * @description Dynamic identity resolver. Bridges global identity with local project context.
  */
 import { Ø1D } from "https://1dkvr.github.io/FrameKit/Human.js";
 Ø1D.attach("FrameKit");
