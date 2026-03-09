387da033b98b18c36549231f2edf3ea2feb82b7c
1Dkvr <1D.KVR.user@gmail.com>
2026-03-09 01:38:58 +0100

    Update AbstractClass.js

--- core/js/AbstractClass.js
+++ core/js/AbstractClass.js
@@ -3,8 +3,9 @@
  * @description Base class that prevents direct instantiation.
  * @version 1.0.0
  * @author 1D
- * @updated 2026.03.08
  * @copyright © 2026 Hold'inCorp. All rights reserved.
+ * @license Apache-2.0
+ * @updated 26.03.08
  */
 
 import { Ø1D } from "../../Humans.js";
